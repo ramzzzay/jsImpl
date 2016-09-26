69f7a3fbdd1a106cb7a55e8431e2db6740b8aafa
ramzzzay <ramzzzay@gmail.com>
2016-09-26 13:40:24 +0300

    Refactoring: format of code

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -1,27 +1,27 @@
 export default class ArrayMethods {
-static forEach(arr, callback) {
-    for(var i=0;i<=arr.length-1;i++) {
-        callback(arr[i]);
+    static forEach(arr, callback) {
+        for (var i = 0; i <= arr.length - 1; i++) {
+            callback(arr[i]);
+        }
     }
-}
 
-static map(arr, callback) {
-    var response = [];
-    for(var i=0;i<=arr.length-1;i++) {
-        response.push(callback(arr[i]));
+    static map(arr, callback) {
+        var response = [];
+        for (var i = 0; i <= arr.length - 1; i++) {
+            response.push(callback(arr[i]));
+        }
+        return response;
     }
-    return response;
-}
 
-static skip(arr, n) {
-    var response = [];
-    for(var i=n;i<=arr.length-1;i++){
-        response.push(arr[i])
+    static skip(arr, n) {
+        var response = [];
+        for (var i = n; i <= arr.length - 1; i++) {
+            response.push(arr[i])
+        }
+        return response;
     }
-    return response;
-}
 
-static take(arr, n) {
-    return arr[n-1];
-}
+    static take(arr, n) {
+        return arr[n - 1];
+    }
 };
