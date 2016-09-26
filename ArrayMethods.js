25a86618154921400c0775ba13a03161b727ee46
ramzzzay <ramzzzay@gmail.com>
2016-09-26 15:49:53 +0300

    Add: Chainable Methods

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -1,27 +1,90 @@
 export default class ArrayMethods {
-	static forEach(arr, callback) {
-		for (var i = 0; i <= arr.length - 1; i++) {
-			callback(arr[i]);
-		}
-	}
+    constructor(array) {
+        this.array = array;
+    }
 
-	static map(arr, callback) {
-		var response = [];
-		for (var i = 0; i <= arr.length - 1; i++) {
-			response.push(callback(arr[i]));
-		}
-		return response;
-	}
+    toString() {
+        return this.array.map(i=>i).toString();
+    }
 
-	static skip(arr, n) {
-		var response = [];
-		for (var i = n; i <= arr.length - 1; i++) {
-			response.push(arr[i])
-		}
-		return response;
-	}
+    forEach(callback) {
+        for (var i = 0; i <= this.array.length - 1; i++) {
+            callback(this.array[i]);
+        }
+    }
 
-	static take(arr, n) {
-		return arr[n - 1];
-	}
+    map(callback) {
+        var response = [];
+        for (var i = 0; i <= this.array.length - 1; i++) {
+            response.push(callback(this.array[i]));
+        }
+        return new ArrayMethods(response);
+    }
+
+    skip(n) {
+        var response = [];
+        for (var i = n; i <= this.array.length - 1; i++) {
+            response.push(this.array[i])
+        }
+        return new ArrayMethods(response);
+    }
+
+    take(n) {
+        var response = [];
+        for(var i = 0; i<= n-1;i++){
+            response.push(this.array[i])
+        }
+        return new ArrayMethods(response);
+    }
+
+    reduce(callback) {
+        'use strict';
+        if (this === null) {
+            throw new TypeError('Array.prototype.reduce called on null or undefined');
+        }
+        if (typeof callback !== 'function') {
+            throw new TypeError(callback + ' is not a function');
+        }
+        var t = Object(this.array), len = t.length >>> 0, k = 0, value;
+        if (arguments.length == 2) {
+            value = arguments[1];
+        } else {
+            while (k < len && !(k in t)) {
+                k++;
+            }
+            if (k >= len) {
+                throw new TypeError('Reduce of empty array with no initial value');
+            }
+            value = t[k++];
+        }
+        for (; k < len; k++) {
+            if (k in t) {
+                value = callback(value, t[k], k, t);
+            }
+        }
+        return new ArrayMethods(value);
+    };
+
+    filter(fun) {
+        'use strict';
+        if (this === void 0 || this === null) {
+            throw new TypeError();
+        }
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (typeof fun !== 'function') {
+            throw new TypeError();
+        }
+        var res = [];
+        var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+        for (var i = 0; i < len; i++) {
+            if (i in t) {
+                var val = t[i];
+                if (fun.call(thisArg, val, i, t)) {
+                    res.push(val);
+                }
+            }
+        }
+        return new ArrayMethods(res);
+    };
 };
