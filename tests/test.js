4134b94ca1049b54ab4284c190342851245957a6
ramzzzay <ramzzzay@gmail.com>
2016-09-27 11:22:15 +0300

    Add: forEach() method test

--- tests/test.js
+++ tests/test.js
@@ -27,3 +27,9 @@ describe('Test 4. ', function() {
         expect(ArrayMethods.chain([1,2,3,4,5,6,7,8,9,10]).take(5).skip(3).value().toString()).toEqual("4,5");
     });
 });
+
+describe('Test 5. ', function() {
+    it('Checking forEach() method: ', function() {
+        expect(ArrayMethods.chain([1,2,3,4,5,6,7,8,9,10]).take(5).skip(3).forEach(i=>console.log(i))).toEqual([4,5].forEach(i=>console.log(i)));
+    });
+});
