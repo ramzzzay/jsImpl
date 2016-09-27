5d5026dcea7736a710b3a919da6a730e17affbf1
ramzzzay <ramzzzay@gmail.com>
2016-09-27 14:28:27 +0300

    Add: logging to tests

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function(config) {
 	// test results reporter to use
 	// possible values: 'dots', 'progress'
 	// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-	reporters: ['progress'],
+	reporters: ['progress','mocha'],
 
 
 	// web server port
@@ -48,6 +48,13 @@ module.exports = function(config) {
 	// level of logging
 	// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 	logLevel: config.LOG_INFO,
+	  client: {
+		  captureConsole: true,
+		  mocha: {
+			  bail: true}
+	  },
+
+
 
 
 	// enable / disable watching file and executing tests whenever any file changes
