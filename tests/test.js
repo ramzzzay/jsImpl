512e742c8d423f778fac40b9a2f4e97128c68880
ramzzzay <ramzzzay@gmail.com>
2016-09-27 11:21:47 +0300

    Add: toString() method test

--- tests/test.js
+++ tests/test.js
@@ -5,19 +5,25 @@
 // import ArrayMethods from "../ArrayMethods.js";
 
 describe('Test 1. ', function() {
-    it('Checking chain&&value methods: ', function() {
+    it('Checking chain()&&value() methods: ', function() {
         expect(ArrayMethods.chain([1,2,3,4,5,6,7,8,9,10]).value()).toEqual([1,2,3,4,5,6,7,8,9,10]);
     });
 });
 
 describe('Test 2. ', function() {
-    it('Checking take method: ', function() {
+    it('Checking take() method: ', function() {
         expect(ArrayMethods.chain([1,2,3,4,5,6,7,8,9,10]).take(1).value()).toEqual([1]);
     });
 });
 
 describe('Test 3. ', function() {
-    it('Checking skip method: ', function() {
+    it('Checking skip() method: ', function() {
         expect(ArrayMethods.chain([1,2,3,4,5,6,7,8,9,10]).take(5).skip(3).value()).toEqual([4,5]);
+    });
+});
+
+describe('Test 4. ', function() {
+    it('Checking toString() method: ', function() {
+        expect(ArrayMethods.chain([1,2,3,4,5,6,7,8,9,10]).take(5).skip(3).value().toString()).toEqual("4,5");
     });
 });
