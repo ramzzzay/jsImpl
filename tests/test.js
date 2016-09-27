612a9fa424aea35f316745bccea9ead661bd760e
ramzzzay <ramzzzay@gmail.com>
2016-09-27 11:11:21 +0300

    Add: skip method test

--- tests/test.js
+++ tests/test.js
@@ -4,16 +4,20 @@
 
 // import ArrayMethods from "../ArrayMethods.js";
 
-describe('Users factory', function() {
-    it('Checking chain method: ', function() {
-        // An intentionally failing test. No code within expect() will never equal 4.
+describe('Test 1. ', function() {
+    it('Checking chain&&value methods: ', function() {
         expect(ArrayMethods.chain([1,2,3,4,5,6,7,8,9,10]).value()).toEqual([1,2,3,4,5,6,7,8,9,10]);
     });
 });
 
-describe('Users factory', function() {
+describe('Test 2. ', function() {
     it('Checking take method: ', function() {
-        // An intentionally failing test. No code within expect() will never equal 4.
         expect(ArrayMethods.chain([1,2,3,4,5,6,7,8,9,10]).take(1).value()).toEqual([1]);
+    });
+});
+
+describe('Test 3. ', function() {
+    it('Checking skip method: ', function() {
+        expect(ArrayMethods.chain([1,2,3,4,5,6,7,8,9,10]).take(5).skip(3).value()).toEqual([4,5]);
     });
 });
